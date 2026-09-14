738c38b160ab07f4ffc3f8117c4265e60f1963d5
sushantpadha <24b1057@iitb.ac.in>
2026-09-14 16:07:46 +0530

    nvim: fix delete to not copy, etc

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,12 +1,24 @@
--- vim.opt.tabstop = 4     -- Number of spaces that a tab counts for (dont need to change)
+-- Indentation
+-- vim.opt.tabstop = 4     -- Number of spaces that a tab counts for (don't need to change)
 vim.opt.shiftwidth = 4     -- Width for auto-indents
 vim.opt.expandtab = true   -- Convert tabs to spaces
-vim.opt.softtabstop = 4    -- Insert/delete 4 spaces when pressing tab/backspace
+vim.opt.softtabstop = 4    -- Insert/delete 4 spaces when pressing Tab/Backspace
 
+-- Cursor
+vim.opt.guicursor = "a:block"
+
+-- Terminal
 -- tnoremap <Esc> <C-\><C-n>
 
--- remap ctrl shift c for copy instead of "+y or y after doing `set clipboard=unnamedplus`
-vim.opt.clipboard = "unnamedplus"  -- assumes a clipboard manager is installed by default
-vim.keymap.set('v', '<C-S-c>', 'y', { noremap = true })
+-- System clipboard
+-- Use the system clipboard for normal yanks/deletes.
+vim.opt.clipboard = "unnamedplus"
 
+-- Ctrl+Shift+C: explicitly copy the visual selection to the system clipboard.
+vim.keymap.set("v", "<C-S-c>", '"+y', { noremap = true })
 
+-- Delete without overwriting the system clipboard.
+-- "_ is the black-hole register.
+vim.keymap.set({ "n", "v" }, "d", '"_d')
+vim.keymap.set({ "n", "v" }, "D", '"_D')
+vim.keymap.set("n", "x", '"_x')
